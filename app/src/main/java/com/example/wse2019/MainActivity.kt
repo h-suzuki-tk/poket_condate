d64f29315c343f4c101fbf5d87c6573cf5a541b4
suzuki.hina <hina.suzuki.298@gmail.com>
2019-06-20 03:27:45 +0900

    タブを追加

--- app/src/main/java/com/example/wse2019/MainActivity.kt
+++ app/src/main/java/com/example/wse2019/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.wse2019
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import android.support.design.widget.TabLayout
 import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* ツールバーをアクションバーとしてセット */
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        // ツールバーをアクションバーとしてセット
+        // -------------------------------------------------------------------------------
+        var toolbar: Toolbar = findViewById(R.id.toolBar)
 
         toolbar.setNavigationIcon(R.mipmap.ic_launcher_round)
+        toolbar.setTitle("ほげほげ")
 
         setSupportActionBar(toolbar)
+        // -------------------------------------------------------------------------------
+
+        // タブをセット
+        // -------------------------------------------------------------------------------
+        var tabLayout: TabLayout = findViewById(R.id.tabLayout)
+
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.top))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.record))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.eval))
+        // -------------------------------------------------------------------------------
     }
 
 }
