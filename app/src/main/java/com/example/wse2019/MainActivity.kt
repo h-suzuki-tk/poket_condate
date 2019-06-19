b8af69eabe62d4aebb19249089b6037c576b5954
suzuki.hina <hina.suzuki.298@gmail.com>
2019-06-20 02:38:03 +0900

    ToolBar用に調整

--- app/src/main/java/com/example/wse2019/MainActivity.kt
+++ app/src/main/java/com/example/wse2019/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.wse2019
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* ツールバーをアクションバーとしてセット */
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher_round)
+
+        setSupportActionBar(toolbar)
     }
+
 }
