6572d27627a1aff4fa20e5b962a1e3760a702a41
suzuki.hina <hina.suzuki.298@gmail.com>
2019-06-21 03:47:12 +0900

    ナビゲーションドロワーの実装

--- app/src/main/java/com/example/wse2019/MainActivity.kt
+++ app/src/main/java/com/example/wse2019/MainActivity.kt
@@ -1,35 +1,58 @@
 package com.example.wse2019
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.support.v7.widget.Toolbar
-import android.support.design.widget.TabLayout
-import android.view.MenuItem
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.design.widget.TabLayout
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
+import android.view.View
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // ツールバーをアクションバーとしてセット
-        // -------------------------------------------------------------------------------
-        var toolbar: Toolbar = findViewById(R.id.toolBar)
-
-        toolbar.setNavigationIcon(R.drawable.baseline_reorder_black_18dp)
-        toolbar.setTitle(R.string.app_name)
-
+        // ツールバーをセット
+        val toolbar: Toolbar = findViewById(R.id.toolBar)
         setSupportActionBar(toolbar)
-        // -------------------------------------------------------------------------------
 
-        // タブをセット
-        // -------------------------------------------------------------------------------
-        var tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        // ナビゲーションメニューをセット
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.navView)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
 
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.top))
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.record))
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.eval))
-        // -------------------------------------------------------------------------------
+    // ナビゲーションメニューの各項目を選択した際の動作
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_top -> {
+
+            }
+            R.id.nav_record -> {
+
+            }
+            R.id.nav_eval -> {
+
+            }
+            R.id.nav_mycondate -> {
+
+            }
+            R.id.nav_regisration_inf_edit -> {
+
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }
