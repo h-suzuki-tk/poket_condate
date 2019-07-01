c9040fbc91e43b39789bf12ffd99e63ecefd275c
suzuki.hina <hina.suzuki.298@gmail.com>
2019-07-01 15:22:51 +0900

    トップ画面とカレンダー画面の仮レイアウトの作成

--- app/src/main/java/com/example/wse2019/CalendarFragment.kt
+++ app/src/main/java/com/example/wse2019/CalendarFragment.kt
@@ -7,14 +7,24 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ListView
 
 
 class CalendarFragment() : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        val v: View = inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        val prevButton: Button = v.findViewById(R.id.prevMonth)
+        val nextButton: Button = v.findViewById(R.id.nextMonth)
+        prevButton.setOnClickListener { /* 動作をここに記述 */ }
+        nextButton.setOnClickListener { /* 動作をここに記述 */ }
+
+        return v
     }
     override fun onAttach(context: Context?) {
         super.onAttach(context)
