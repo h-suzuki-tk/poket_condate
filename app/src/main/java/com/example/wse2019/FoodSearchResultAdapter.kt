ff86afe0d5820124be9369ffd48b4aba931caf70
suzuki.hina <hina.suzuki.298@gmail.com>
2019-08-12 20:37:44 +0900

    お気に入り登録・削除機能を追加

--- app/src/main/java/com/example/wse2019/FoodSearchResultAdapter.kt
+++ app/src/main/java/com/example/wse2019/FoodSearchResultAdapter.kt
@@ -1,28 +1,32 @@
 package com.example.wse2019
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteException
 import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.example.sample.DBContract
+import com.example.sample.SampleDBOpenHelper
 
 class FoodSearchResultAdapter(context: Context) : BaseAdapter() {
-    val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    private val context = context
 
+    // ----- [表示用]品目データクラス -----
     data class Food(
-        val id: Int,
-        val iconID: Int,
-        val name: String,
+        val id      : Int,
+        val iconID  : Int = R.drawable.ic_menu_gallery,
+        val name    : String,
+        val category: Int,
         var favorite: Int
     )
 
     val favorite_resourceID= listOf<Int>(android.R.drawable.btn_star_big_off, android.R.drawable.btn_star_big_on)
-    var foods = mutableListOf<Food>(
-        Food(1, R.drawable.ic_menu_gallery, "いくらごはん", 0),
-        Food(2, R.drawable.ic_menu_manage, "うなぎごはん", 1),
-        Food(3, R.drawable.ic_menu_camera, "肉ごはん", 0)) // 初期化
+    var foods = mutableListOf<Food>()   // [表示用]品目プロパティ
 
     override fun getCount(): Int {
         return foods.size
@@ -53,7 +57,7 @@ class FoodSearchResultAdapter(context: Context) : BaseAdapter() {
         favorite.setImageResource(favorite_resourceID[foods[position].favorite])
         favorite.setOnClickListener {
             parent.performItemClick(v, position, R.id.favoriteButton.toLong())
-            favorite.setImageResource(favorite_resourceID[foods[position].favorite])}
+        }
 
         // 追加ボタンのセット
         val add: Button = v.findViewById(R.id.addButton)
@@ -68,7 +72,7 @@ class FoodSearchResultAdapter(context: Context) : BaseAdapter() {
     fun switchFavorite(position: Int) {
         val current: Int = foods[position].favorite
 
-        // ----- 表示用プロパティの修正 ------
+        // ----- [表示用]プロパティの修正 ------
         foods[position].favorite = when (current) {
             0       -> 1
             1       -> 0
@@ -76,9 +80,82 @@ class FoodSearchResultAdapter(context: Context) : BaseAdapter() {
         }
 
         // ----- データベースの修正 -----
+        val db = SampleDBOpenHelper(context).writableDatabase
+        val foodTable = DBContract.Food
 
-        /***** ここに記述 *****/
+        val values = ContentValues()
+        values.put("favorite", foods[position].favorite)
+        db.update(
+            "foods",
+            values,
+            "id = ${foods[position].id}",
+            null)
 
+        // うまくいかない
+        /*
+        db.updateRecord(
+            tablename       = foodTable.TABLE_NAME,
+            column          = arrayOf(
+                foodTable.FAVORITE),
+            convert         = arrayOf(
+                "${foods[position].favorite}"),
+            condition       = "${foodTable.ID} -> ?",
+            selectionArgs   = arrayOf(
+                "${foods[position].id}")
+        )
+        */
+    }
+
+    // ------------------------------------------------------------
+    //  searchFoods - 食べ物を検索 -
+    // ------------------------------------------------------------
+    fun searchFoods(
+        name       : String?    = null,
+        favorite   : Int        = 0,
+        category   : Int        = 0
+    ) {
+        val db = SampleDBOpenHelper(context)
+        val foodTable = DBContract.Food
+        var condition = ""
+
+        // ----- 条件の設定 -----
+        if (name != null && name.isNotBlank() && name.isNotEmpty()) {
+            condition += "${foodTable.NAME} like '%${name}%'"
+        }
+        if (favorite != 0) {
+            if (condition.isNotEmpty()) { condition += " and " }
+            condition += "${foodTable.FAVORITE} = ${favorite}"
+        }
+        if (category != 0) {
+            if (condition.isNotEmpty()) { condition += " and " }
+            condition += "${foodTable.CATEGORY} = ${category}"
+        }
+
+        // ----- 検索 -----
+        val result: List<String> = db.searchRecord(
+            tableName   = foodTable.TABLE_NAME,
+            column      = arrayOf(
+                foodTable.ID,
+                foodTable.NAME,
+                foodTable.CATEGORY,
+                foodTable.FAVORITE),
+            condition   = when (condition.isEmpty()) {
+                true    -> null
+                false   -> condition }
+        ) ?: throw NullPointerException("searchRecord was failed.")
+
+        // ----- 格納 -----
+        this.foods.clear()
+        var i = 0
+        while (i < result.size) {
+            this.foods.add(Food(
+                id          = result[i++].toInt(),
+                name        = result[i++],
+                category    = result[i++].toInt(),
+                favorite    = result[i++].toInt()
+                ))
+        }
+        notifyDataSetChanged()
     }
 
 }
