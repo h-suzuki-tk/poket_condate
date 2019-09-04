3b1db8c738fe86d11fb108ccf61c09accf68d637
suzuki.hina <hina.suzuki.298@gmail.com>
2019-09-04 15:46:09 +0900

    [トップ画面] 枠組みを作成

--- app/src/main/java/com/example/wse2019/TopFragment.kt
+++ app/src/main/java/com/example/wse2019/TopFragment.kt
@@ -8,15 +8,68 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 
 
 class TopFragment() : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_top, container, false)
+        val view = inflater.inflate(R.layout.fragment_top, container, false)
+
+        // --------------------------------------------------
+        //  今週の得点
+        // --------------------------------------------------
+        val score = view.findViewById<TextView>(R.id.ft_scoreTextView)
+        score.apply {
+            //text = /* score */
+        }
+
+        // --------------------------------------------------
+        //  最もおすすめな品目
+        // --------------------------------------------------
+        val recommendFood = view.findViewById<LinearLayout>(R.id.ft_recommendFood)
+        val image = view.findViewById<ImageView>(R.id.ft_recommendImageView)
+        val name = view.findViewById<TextView>(R.id.ft_foodNameTextView)
+        recommendFood.apply {
+            setOnClickListener {
+                // 献立登録画面へ (余裕があれば、その前に品目の詳細を表示する)
+            }
+        }
+        image.apply {
+            //setImageResource(recommendFood.first.id)
+        }
+        name.apply {
+            //text = recommendFood.first.name
+        }
+
+        // --------------------------------------------------
+        //  そのほかのおすすめ品目
+        // --------------------------------------------------
+        val otherRecommendFood = view.findViewById<ListView>(R.id.ft_otherRecommendFoodListView)
+        otherRecommendFood.apply {
+            //adapter = recommendFood.getAdapter()
+            setOnItemClickListener { view, parent, position, id ->
+                //献立登録画面へ (余裕があれば、その前に品目の詳細を表示する)
+            }
+        }
+
+        // --------------------------------------------------
+        //  もっと見る！ボタン
+        // --------------------------------------------------
+        val showMore = view.findViewById<LinearLayout>(R.id.ft_showMoreTextView)
+        showMore.apply {
+            setOnClickListener {
+                showMore.visibility = View.GONE
+                //recommendFood.showMore(10)
+            }
+        }
+
+        return view
     }
+
     override fun onAttach(context: Context?) {
         super.onAttach(context)
     }
