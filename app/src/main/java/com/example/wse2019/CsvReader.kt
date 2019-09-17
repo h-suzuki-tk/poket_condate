5e7a4ce8bb97bbbcdb322ef80770d9e4ec2d641d
KaijiKonoshima <s1611436@u.tsukuba.ac.jp>
2019-09-17 15:53:15 +0900

    材料用のcsv読み取り関数を実装

---
+++ app/src/main/java/com/example/wse2019/CsvReader.kt
@@ -0,0 +1,61 @@
+package com.example.wse2019
+
+import android.content.*
+import com.example.sample.SampleDBOpenHelper
+import com.example.sample.Table
+import java.io.*
+import android.util.Log
+
+
+//csvを1行ずつ読み取り、splitしてInsertする関数
+/*csvファイルの行は　名前,炭水化物,脂質,タンパク質,ビタミン,ミネラル,カロリー,量,単位,アレルゲン(アレルギーが出ない食べ物は一説によるとないらしいのでいまはすべて1にしている),区分
+  という形で記述する  また、csvファイルはShift-JISにすること(文字化けするため)*/
+    fun csvImportHelper(context: Context,db:SampleDBOpenHelper,filename:String) {
+        val assetManager = context.getResources().getAssets()
+        try {
+            // CSVファイルの読み込み
+
+            val inputStream = assetManager.open(filename)
+            val inputStreamReader = InputStreamReader(inputStream,"SJIS")
+            val bufferReader = BufferedReader(inputStreamReader)
+            var line: String?
+            do{
+
+                line = bufferReader.readLine()
+                if(line==null){
+                    break
+                }
+
+                //カンマ区切りで１つづつ配列に入れる
+                Log.d("line:",line)
+                var RowData = line.split(",")
+
+                var i=0
+
+                var data=ArrayList<String>()
+
+                while(i<RowData.size){
+                    if(RowData[i]=="Tr" || RowData[i]=="-"){
+                        data.add("0")
+                    }else{
+                        data.add(RowData[i])
+                    }
+                    i++
+                }
+
+                db.insertRecord(
+                    Table.Ingredient(
+                        data[0].toString(), data[1].toFloat(), data[2].toFloat(), data[3].toFloat(), null, 0f,
+                        data[6].toFloat(), data[7].toFloat(), data[8].toFloat(), data[9].toString(),1, data[11].toInt()
+                    )
+                )
+
+
+
+            }while(true)
+            bufferReader.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
--- app/src/main/java/com/example/wse2019/Initializer.kt
+++ app/src/main/java/com/example/wse2019/Initializer.kt
@@ -2,6 +2,7 @@ package com.example.sample
 
 import android.content.Context
 import android.util.Log
+import com.example.wse2019.csvImportHelper
 
 //db作り直しからデフォルト値入れるまで
 fun initializer(context: Context) {
@@ -16,4 +17,11 @@ fun initializer(context: Context) {
     default.forEach {
         DB.insertRecord(it)
     }
+    csvImportHelper(context,DB,"Ingredients/014.csv")
+    csvImportHelper(context,DB,"Ingredients/011.csv")
+    csvImportHelper(context,DB,"Ingredients/015.csv")
+    csvImportHelper(context,DB,"Ingredients/016.csv")
+    csvImportHelper(context,DB,"Ingredients/017.csv")
+    csvImportHelper(context,DB,"Ingredients/018.csv")
+
 }
